7a114cb649404861d2ed4d3bf07370420f679ad7
Mason <mason@paymentwall.com>
2019-03-01 16:16:25 +0700

    Create FP Module for Magento 1

---
+++ src/app/code/community/FasterPay/FasterPay/Block/Checkout/Form/Method/Fasterpay.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ *
+ * Class FasterPay_FasterPay_Block_Checkout_Form_Method_Fasterpay
+ */
+class FasterPay_FasterPay_Block_Checkout_Form_Method_Fasterpay extends FasterPay_FasterPay_Block_Checkout_Form_Method_Abstract
+{
+    /**
+     * Set template for block
+     * @return void
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setPaymentModelName('fasterpay');
+    }
+
+    function getPaymentForm()
+    {
+        $order = $this->getOrder();
+        $return = array(
+            'content' => '',
+            'status' => false
+        );
+
+        if ($order) {
+            try {
+                $model = $this->getPaymentModel();
+                $paymentForm = $model->getPaymentForm($order);
+
+                // Get widget iframe
+                $return['content'] = $paymentForm;
+                $return['status'] = true;
+            } catch (Exception $e) {
+                Mage::logException($e);
+                $return['content'] = Mage::helper('fasterpay')->__('Errors, Please try again!');
+            }
+        } else {
+            $return['content'] = Mage::helper('fasterpay')->__('Order invalid'); //should redirect back to homepage
+        }
+
+        return $return;
+    }
+
+    /**
+     * Get last order
+     */
+    protected function getOrder()
+    {
+        if (!$this->_order) {
+            $session = Mage::getSingleton('checkout/session');
+            $this->_order = $this->loadOrderById($session->getLastRealOrderId());
+        }
+        return $this->_order;
+    }
+
+    protected function loadOrderById($orderId)
+    {
+        return Mage::getModel('sales/order')->loadByIncrementId($orderId);
+    }
+}
---
+++ src/app/code/community/FasterPay/FasterPay/Block/Checkout/Info/Method/Fasterpay.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *
+ * Class FasterPay_FasterPay_Block_Checkout_Info_Method_Fasterpay
+ */
+class FasterPay_FasterPay_Block_Checkout_Info_Method_Fasterpay extends Mage_Payment_Block_Info
+{
+    protected function _construct()
+    {
+        parent::_construct();
+    }
+}
